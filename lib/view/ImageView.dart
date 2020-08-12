75c62e0116ca7f422c5ec524d26cd8468b42b46a
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-08-12 06:03:55 +0100

    started with imageView screen

--- lib/view/ImageView.dart
+++ lib/view/ImageView.dart
@@ -1,4 +1,13 @@
+import 'dart:io' as io;
+import 'dart:typed_data';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ImageView extends StatefulWidget {
@@ -30,10 +39,56 @@ class _ImageViewState extends State<ImageView> {
             child: Container(
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
+              child: kIsWeb
+                  ? Image.network(widget.imgPath, fit: BoxFit.cover)
+                  : CachedNetworkImage(
+                      imageUrl: widget.imgPath,
+                      placeholder: (context, url) => Container(
+                        color: Color(0xfff5f8fd),
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                InkWell(
+                  onTap: () {
+                    if (kIsWeb) {
+                      _launchUrl(widget.imgPath);
+                    } else {
+                      _save;
+                    }
+                  },
+                ),
+              ],
             ),
           )
         ],
       ),
     );
+  }
+
+  _save() async {
+    await _askPermission();
+    var response = await Dio().get(widget.imgPath,
+        options: Options(responseType: ResponseType.bytes));
+    final result =
+        await ImageGallerySaver.saveImage(Uint8List.fromList(response.data));
+    print(result);
+    Navigator.pop(context);
+  }
+
+  _askPermission() async {
+    if (io.Platform.isIOS) {
+      await PermissionHandler().requestPermissions([PermissionGroup.photos]);
+    } else {
+      await PermissionHandler().checkPermissionStatus(PermissionGroup.storage);
+    }
   }
 }
